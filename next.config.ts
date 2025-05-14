10909e8616fda3c8e4afa701cd6689c0a113544b
guntarahmbl <guntarahambali3@gmail.com>
2025-05-14 15:12:33 +0700

    fix: @ path

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,24 @@
 // next.config.ts
 // @ts-ignore
 import withPWA from "next-pwa";
+import path from "path";
+import type { NextConfig } from "next";
 
-const config = withPWA({
+const nextConfig: NextConfig = {
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias ?? {}),
+      "@": path.resolve(__dirname),
+    };
+    return config;
+  },
+};
+
+export default withPWA({
+  ...nextConfig,
   pwa: {
-    dest: "public", // Direktori untuk file PWA
+    dest: "public",
     register: true,
     skipWaiting: true,
   },
 });
-
-export default config;
